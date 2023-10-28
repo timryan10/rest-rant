a9a79e5db7a33fd436c0061f5ac15ebb8cf7313d
Tim <rtim10@ymail.com>
2023-10-28 12:29:49 -0400

    adding bootstrap

--- Views/error404.jsx
+++ Views/error404.jsx
@@ -6,6 +6,10 @@ function erro404 () {
         <Def>
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
+                <div>
+                    <img src="/images/penguin.jpg" width= "400" height="400" alt="penguin" />
+                    <div>Photo by <a href="https://unsplash.com/@corneliusventures">Cornelius Ventures</a>on <a href="https://unsplash.com/">Unsplash</a></div>
+                </div>
                 <p>Oops, sorry, we can't find this page!</p>
             </main>
         </Def>
--- Views/home.jsx
+++ Views/home.jsx
@@ -6,6 +6,12 @@ function home () {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img src="/images/chia-fruit-drink.jpg" width="400" height= "400" alt="chia fruit drink" />
+                    <div>
+                        Photo by <a href="https://unsplash.com/@cravethebenefits">Brenda Gonzalez</a> on <a href="https://unsplash.com/">Unsplash</a>
+                    </div>
+                </div>
                 <a href="/places">
                     <button className='btn-primary'>Places Page</button>
                 </a>
