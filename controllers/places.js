b343a508880fd8cfa97f6bec1888a2e2a939d597
Tim <rtim10@ymail.com>
2023-11-01 21:04:14 -0400

    part 8 completed

--- controllers/places.js
+++ controllers/places.js
@@ -1,24 +1,27 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
+const db = require('../models')
 
 router.get('/', (req, res) => {      
-    res.render('places/index', {places})
+    db.Place.find()
+    .then((places) => {
+      res.render('places/index', {places})
+    })
+    .catch(err => {
+      console.log(err)
+      res.render('error404')
+    })
 })
 
 router.post('/', (req, res) => {
-  if (!req.body.pic) {
-    req.body.pic = 'http://placekitten.com/400/400'
-  }
-
-  if (!req.body.city) {
-    req.body.city = 'Anytown'
-  }
-
-  if (!req.body.state) {
-    req.body.state = 'USA'
-  }
-  places.push(req.body)
-  res.redirect('/places')
+  db.Place.create(req.body)
+  .then(() => {
+    res.redirect('places')
+  })
+  .catch(err => {
+    console.log('err', err)
+    res.render('error404')
+  })
 })
 
 router.get('/new', (req, res) => {
@@ -40,20 +43,14 @@ router.delete('/:id', (req, res) => {
 })
 
 router.get('/:id', (req, res) => {
-  let id =  Number(req.params.id)
-  if (isNaN(id)) {
+  db.Place.findById(req.param.id)
+  .then(place => {
+    res.render('places/show', {place})
+  })
+  .catch(err => {
+    console.log('err', err)
     res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    let place = places[req.params.id]
-    res.render('places/show', {
-      place: place,
-      index: id
-    })
-  }
+  })
 })
 
 router.put('/:id', (req, res) => {
@@ -89,3 +86,4 @@ router.delete('/id/:rantId', (req, res) => {
 })
 
 module.exports = router
+
--- models/places.js
+++ models/places.js
@@ -1,3 +1,14 @@
+const mongoose = require('mongoose')
+
+const placeSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  pic: String,
+  cuisines: {type: String, required: true},
+  city: {type: String, default: 'Anytown'},
+  state: {type: String, default: 'Anytown'},
+  founded: Number
+})
+
 module.exports = [{
     name: 'H-Thai-ML',
     city: 'Seattle',
@@ -11,3 +22,5 @@ module.exports = [{
     cuisines: 'Coffee, Bakery',
     pic: '/images/coffee-pic.jpg'
   }]
+
+  module.exports = mongoose.model('Place', placeSchema)
