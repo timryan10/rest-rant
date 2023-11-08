9a63f54d541e32e2264f3410b75bf802cb5ec500
Tim <rtim10@ymail.com>
2023-11-07 21:11:27 -0500

    Comment seed data

---
+++ seeder/seed-comments.js
@@ -0,0 +1,20 @@
+const db = require('../models')
+
+async function seed() {
+    let place = await db.Place.findOne({name: 'H-Thai-ML'})
+
+    let comment = await db.Commment.create({
+        author: 'Famished Fran',
+        rant: false,
+        stars: 5.0,
+        content: 'Wow, simply amazing! Highly recommended!'
+    })
+
+    place.comments.push(comment.id)
+
+    await place.save()
+
+    process.exit()
+}
+
+seed()
